6a0e1a36dfa674d65b43ab6ea2ac9023cef9255b
Shen <shenqi.zhang77@gmail.com>
2026-05-16 17:45:24 +1000

    ISaveFormat finished
    Code that defines save and load format. Connect with Json save and Text save.

--- BoardGames/SaveLoadManager/ISaveFormat.cs
+++ BoardGames/SaveLoadManager/ISaveFormat.cs
@@ -1,1 +1,8 @@
 namespace BoardGames.SaveLoadManager;
+
+//Define save and load format.
+public interface ISaveFormat
+{
+    void Save(GameSaveState saveState, string filePath); //Save game state to file path.
+    GameSaveState Load(string filePath); //Load game state from file path.
+}
